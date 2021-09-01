90f8efeb3a8ae1089b2695474e5ec4f520b219e5
Mahmoud Bekheet <mahmoud.bekheet63@gmail.com>
2021-09-01 17:03:51 +0200

    ch

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  url_api: "http://pixelserver-001-site12.ctempurl.com/api/",
-  url_image: "http://pixelserver-001-site12.ctempurl.com/images/",
-};
